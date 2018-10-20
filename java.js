38c4e39f32f22b06253756ea3794fb30ee6fd6e4
root <root@DESKTOP-9VPKLSN.localdomain>
2018-10-20 02:19:04 +0000

    correção de bugs

--- java.js
+++ java.js
@@ -20,11 +20,9 @@ let empate = 0;
 function gerencia(element, i, j){
     if(turno == 0){
         bola(element,i,j);
-        turno = 1;
     }
     else{
         x(element,i,j);
-        turno = 0;
     }
 }
 function limpa(){
@@ -45,118 +43,124 @@ function limpa(){
     win = 0;
 }
 function bola(element,i,j){
+    if(mat[i][j] != 1 && mat[i][j] != 2 && win == 0){
 
-    mat[i][j] = 1;  // colocando o valor de bola na matriz
-    let flag_linha = true;  // flag que ficara falsa caso algo diferente de bola seja encontada na linha
-    let flag_coluna = true; // flag que ficara falsa caso algo diferente de bola seja encontada na coluna
-    let flag_diag_prin = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal principal
-    let flag_diag_sec = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal secundaria
-    let flag_empate = true;
-    
-    for(i = 0 ; i < 3 && flag_linha == true && flag_coluna == true; i++){
-        for(j=0;j<3;j++){
-            if(mat[i][j] != 1){
-                flag_linha = false; // se algo diferente de bola foi encontrado na linha i flag_linha = false
-            }
-            if(mat[j][i] != 1){
-                flag_coluna = false; // se algo diferente de bola foi encontrado na coluna j flag_coluna = false
-            }
-            if(i == j){
+        turno = 1;
+        mat[i][j] = 1;  // colocando o valor de bola na matriz
+        let flag_linha = true;  // flag que ficara falsa caso algo diferente de bola seja encontada na linha
+        let flag_coluna = true; // flag que ficara falsa caso algo diferente de bola seja encontada na coluna
+        let flag_diag_prin = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal principal
+        let flag_diag_sec = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal secundaria
+        let flag_empate = true;
+        
+        for(i = 0 ; i < 3 && flag_linha == true && flag_coluna == true; i++){
+            for(j=0;j<3;j++){
                 if(mat[i][j] != 1){
-                    flag_diag_prin = false; // se algo diferente de bola foi encontrado na diagonal principal flag_diag_princ = false
+                    flag_linha = false; // se algo diferente de bola foi encontrado na linha i flag_linha = false
+                }
+                if(mat[j][i] != 1){
+                    flag_coluna = false; // se algo diferente de bola foi encontrado na coluna j flag_coluna = false
+                }
+                if(i == j){
+                    if(mat[i][j] != 1){
+                        flag_diag_prin = false; // se algo diferente de bola foi encontrado na diagonal principal flag_diag_princ = false
+                    }
+                }
+                if(j == ((3 - 1) - i)){
+                    if(mat[i][j] != 1){
+                        flag_diag_sec = false; // se algo diferente de bola foi encontrado na diagonal secundaria flag_diag_sec = false
+                    }
+                }
+                if(mat[i][j] == 0){
+                    flag_empate = false;
                 }
             }
-            if(j == ((3 - 1) - i)){
-                if(mat[i][j] != 1){
-                    flag_diag_sec = false; // se algo diferente de bola foi encontrado na diagonal secundaria flag_diag_sec = false
-                }
+            if(flag_linha == true || flag_coluna == true){ // caso alguma linha ou coluna esteja completa alertar o jogador sobre a vitoria
+                alert("o jogador 1 ganhou");
+                flag_linha = false;
+                flag_coluna = false;
+                flag_diag_prin = false;
+                flag_diag_sec = false;
+                win = 1;
+                vitorias_jogador_1++;
             }
-            if(mat[i][j] == 0){
-                flag_empate = false;
+            else{       // senao reinicia as flags e testa de novo
+                if(i != 2){ 
+                    flag_linha = true;
+                    flag_coluna = true;
+                    
+                }
             }
         }
-        if(flag_linha == true || flag_coluna == true){ // caso alguma linha ou coluna esteja completa alertar o jogador sobre a vitoria
+        if((flag_diag_prin == true || flag_diag_sec == true)){  // se alguma diagonal estiver completa alerte o jogador sobre a vitoria
             alert("o jogador 1 ganhou");
-            flag_linha = false;
-            flag_coluna = false;
-            flag_diag_prin = false;
-            flag_diag_sec = false;
             win = 1;
             vitorias_jogador_1++;
         }
-        else{       // senao reinicia as flags e testa de novo
-            if(i != 2){ 
-                flag_linha = true;
-                flag_coluna = true;
-                
-            }
+        if(flag_empate == true && win == false){
+            alert("deu velha");
+            empate++;
         }
+        document.getElementById(element).style.backgroundImage = "url(o.png)"; // alterar a imagem de cada bloco da matriz
     }
-    if((flag_diag_prin == true || flag_diag_sec == true)){  // se alguma diagonal estiver completa alerte o jogador sobre a vitoria
-        alert("o jogador 1 ganhou");
-        win = 1;
-        vitorias_jogador_1++;
-    }
-    if(flag_empate == true && win == false){
-        alert("deu velha");
-        empate++;
-    }
-    document.getElementById(element).style.backgroundImage = "url(o.png)"; // alterar a imagem de cada bloco da matriz
 }
 function x(element,i,j){
-    turno = 0;
-    mat[i][j] = 2;  // colocando o valor de bola na matriz
-    let flag_linha = true;  // flag que ficara falsa caso algo diferente de bola seja encontada na linha
-    let flag_coluna = true; // flag que ficara falsa caso algo diferente de bola seja encontada na coluna
-    let flag_diag_prin = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal principal
-    let flag_diag_sec = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal secundaria
+    if(mat[i][j] != 1 && mat[i][j] != 2 && win == 0){
 
-    for(i = 0 ; i < 3 && flag_linha == true && flag_coluna == true; i++){
-        for(j=0;j<3;j++){
-            if(mat[i][j] != 2){
-                flag_linha = false; // se algo diferente de bola foi encontrado na linha i flag_linha = false
-            }
-            if(mat[j][i] != 2){
-                flag_coluna = false; // se algo diferente de bola foi encontrado na coluna j flag_coluna = false
-            }
-            if(i == j){
+        turno = 0;
+        mat[i][j] = 2;  // colocando o valor de bola na matriz
+        let flag_linha = true;  // flag que ficara falsa caso algo diferente de bola seja encontada na linha
+        let flag_coluna = true; // flag que ficara falsa caso algo diferente de bola seja encontada na coluna
+        let flag_diag_prin = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal principal
+        let flag_diag_sec = true; // flag que ficara falsa caso algo diferente de bola seja encontada na diagonal secundaria
+        
+        for(i = 0 ; i < 3 && flag_linha == true && flag_coluna == true; i++){
+            for(j=0;j<3;j++){
                 if(mat[i][j] != 2){
-                    flag_diag_prin = false; // se algo diferente de bola foi encontrado na diagonal principal flag_diag_princ = false
+                    flag_linha = false; // se algo diferente de bola foi encontrado na linha i flag_linha = false
+                }
+                if(mat[j][i] != 2){
+                    flag_coluna = false; // se algo diferente de bola foi encontrado na coluna j flag_coluna = false
+                }
+                if(i == j){
+                    if(mat[i][j] != 2){
+                        flag_diag_prin = false; // se algo diferente de bola foi encontrado na diagonal principal flag_diag_princ = false
+                    }
+                }
+                if(j == ((3 - 1) - i)){
+                    if(mat[i][j] != 2){
+                        flag_diag_sec = false; // se algo diferente de bola foi encontrado na diagonal secundaria flag_diag_sec = false
+                    }
+                }
+                if(mat[i][j] == 0){
+                    flag_empate = false;
                 }
             }
-            if(j == ((3 - 1) - i)){
-                if(mat[i][j] != 2){
-                    flag_diag_sec = false; // se algo diferente de bola foi encontrado na diagonal secundaria flag_diag_sec = false
-                }
+            if(flag_linha == true || flag_coluna == true){ // caso alguma linha ou coluna esteja completa alertar o jogador sobre a vitoria
+                alert("o jogador 2 ganhou");
+                flag_linha = false;
+                flag_coluna = false;
+                flag_diag_prin = false;
+                flag_diag_sec = false;
+                win = 1;
+                vitorias_jogador_2++;
             }
-            if(mat[i][j] == 0){
-                flag_empate = false;
+            else{       // senao reinicia as flags e testa de novo
+                if(i != 2){ 
+                    flag_linha = true;
+                    flag_coluna = true;
+                }
             }
         }
-        if(flag_linha == true || flag_coluna == true){ // caso alguma linha ou coluna esteja completa alertar o jogador sobre a vitoria
+        if((flag_diag_prin == true || flag_diag_sec == true)){  // se alguma diagonal estiver completa alerte o jogador sobre a vitoria
             alert("o jogador 2 ganhou");
-            flag_linha = false;
-            flag_coluna = false;
-            flag_diag_prin = false;
-            flag_diag_sec = false;
-            win = 1;
             vitorias_jogador_2++;
+            win = 1;
         }
-        else{       // senao reinicia as flags e testa de novo
-            if(i != 2){ 
-                flag_linha = true;
-                flag_coluna = true;
-            }
+        if(flag_empate == true && win == false){
+            alert("deu velha");
+            empate++;
         }
+        document.getElementById(element).style.backgroundImage = "url(x.png)"; // alterar a imagem de cada bloco da matriz
     }
-    if((flag_diag_prin == true || flag_diag_sec == true)){  // se alguma diagonal estiver completa alerte o jogador sobre a vitoria
-        alert("o jogador 2 ganhou");
-        vitorias_jogador_2++;
-        win = 1;
-    }
-    if(flag_empate == true && win == false){
-        alert("deu velha");
-        empate++;
-    }
-    document.getElementById(element).style.backgroundImage = "url(x.png)"; // alterar a imagem de cada bloco da matriz
 }
